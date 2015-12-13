863fd8a7031a3707709cababab3fd932358ccc0f
tkingless <t.kingless@yahoo.com>
2015-12-13 16:38:48 +0800

    init commit

---
+++ src/com/tkk/ChineseMFrate/ChineseMFrate.java
@@ -0,0 +1,14 @@
+package com.tkk.ChineseMFrate;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.*;
+
+public class ChineseMFrateMain {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("hihi");
+}
