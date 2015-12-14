f89ff8832058181b8188b8050871e16bc906442a
tkingless <t.kingless@yahoo.com>
2015-12-14 20:25:04 +0800

    has some bug, question mark

--- src/com/tkk/ChineseMFrate/ChineseMFrateMain.java
+++ src/com/tkk/ChineseMFrate/ChineseMFrateMain.java
@@ -10,21 +10,45 @@ public class ChineseMFrateMain {
 
 		System.out.println("hihi");
 
+		int No_Boy = 0;
+		int No_Girl = 0;
+
 		Random ran = new Random();
 
 		FileManager filewriter = new FileManager("result.dat");
 
-		for(int i = 0; i < 100000; i++){
+		for(int i = 0; i < 10000; i++){
 			//filewriter.Append(Integer.toString(ran.nextInt(42)+1));
 			//filewriter.Append(",");
 
 			//generating [1,42]
-			filewriter.AppendOnNewLine(Integer.toString(ran.nextInt(42)+1),false);
+			//filewriter.AppendOnNewLine(Integer.toString(ran.nextInt(42)+1),false);
+
+			while(true) {
+				int randomNo = ran.nextInt(41)+1;
+
+				filewriter.AppendOnNewLine(Integer.toString(randomNo));
+				if((randomNo % 2) == 0){
+					No_Girl ++;	
+					//System.out.println("Girl plus: " + randomNo);
+				}
+				else {
+					No_Boy ++;
+					//System.out.println("Boy plus: " + randomNo);
+					break;
+				}
+
+			}
 		}
 
 		filewriter.Close();
 
 		System.out.println("byebye");
+		if(No_Girl != 0){
+			System.out.println("M/F: " + (float) No_Boy/No_Girl);
+			System.out.println("Boy: " + No_Boy);
+			System.out.println("Girl: " + No_Girl);
+		}
 
 	}
 }
