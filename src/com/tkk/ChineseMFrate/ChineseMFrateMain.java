df4c4c7552062f2d3289d9398307d12ba216bc6e
tkingless <t.kingless@yahoo.com>
2015-12-14 11:05:22 +0800

    checked uniform distribution

--- src/com/tkk/ChineseMFrate/ChineseMFrateMain.java
+++ src/com/tkk/ChineseMFrate/ChineseMFrateMain.java
@@ -1,8 +1,6 @@
 package com.tkk.ChineseMFrate;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.*;
 
@@ -11,5 +9,22 @@ public class ChineseMFrateMain {
 	public static void main(String[] args) throws IOException {
 
 		System.out.println("hihi");
-        }
+
+		Random ran = new Random();
+
+		FileManager filewriter = new FileManager("result.dat");
+
+		for(int i = 0; i < 100000; i++){
+			//filewriter.Append(Integer.toString(ran.nextInt(42)+1));
+			//filewriter.Append(",");
+
+			//generating [1,42]
+			filewriter.AppendOnNewLine(Integer.toString(ran.nextInt(42)+1),false);
+		}
+
+		filewriter.Close();
+
+		System.out.println("byebye");
+
+	}
 }
